59101a3dcdcde2fbb144ae4cb1d77cd05f8faf6c
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2024-07-01 19:28:35 -0400

    add captionImage function and model

--- functions/assembly/classify.ts
+++ functions/assembly/classify.ts
@@ -1,22 +1,32 @@
 import { models } from "@hypermode/functions-as";
-import { ClassificationModel } from "@hypermode/models-as/models/experimental/classification";
+import { Model } from "@hypermode/models-as";
 
-// This function classifies an issue based on its title.
-// The model used is tailored for GitHub issues.
-// See https://huggingface.co/AntoineMC/distilbart-mnli-github-issues
 
-export function classifyIssue(
-  id: string,
-  title: string,
-  description: string,
-): string {
-  console.log(`Classifying issue ${id}`);
-  const summary = `${title}\n${description}`;
+@json
+class ImageCaptioningInput {
+  instances!: string[];
+}
 
-  const model = models.getModel<ClassificationModel>("issue-classifier");
-  const input = model.createInput([summary]);
+
+@json
+class ImageCaptioningOutput {
+  predictions!: string[];
+}
+
+export class ImageCaptioningModel extends Model<
+  ImageCaptioningInput,
+  ImageCaptioningOutput
+> {
+  createInput(instances: string[]): ImageCaptioningInput {
+    return <ImageCaptioningInput>{ instances };
+  }
+}
+
+export function captionImage(b64image: string): string {
+  console.log(`size: ${b64image.length}`);
+
+  const model = models.getModel<ImageCaptioningModel>("image-captioning");
+  const input = model.createInput([b64image]);
   const output = model.invoke(input).predictions[0];
-
-  console.log(`Issue ${id} classified as ${output.label}`);
-  return output.label;
+  return output;
 }
